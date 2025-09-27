5cf1a312451ebaa4695d2ca0842664d14032dafd
vikas sharma <vikasjavadev311@gmail.com>
2025-09-28 02:45:35 +0530

    chore: initial commit with Spring Boot OpenAI integration

---
+++ openai/openai/src/main/java/com/lcwv/openai/controller/ChatController.java
@@ -0,0 +1,22 @@
+package com.lcwv.openai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ChatController {
+   private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam("message") String message){
+        return chatClient.prompt().call().content();
+    }
+}
