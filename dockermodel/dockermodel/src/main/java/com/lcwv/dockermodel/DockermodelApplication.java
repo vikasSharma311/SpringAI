8b52f4c6cee62fa111ef0dcbcefb5fb24d9635f8
vikas sharma <vikasjavadev311@gmail.com>
2025-10-02 18:45:23 +0530

    feat: integrate Docker model with Gemma 3 and test chat API

---
+++ dockermodel/dockermodel/src/main/java/com/lcwv/dockermodel/DockermodelApplication.java
@@ -0,0 +1,13 @@
+package com.lcwv.dockermodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockermodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockermodelApplication.class, args);
+	}
+
+}
---
+++ dockermodel/dockermodel/src/test/java/com/lcwv/dockermodel/DockermodelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwv.dockermodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockermodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
