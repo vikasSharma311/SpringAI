162e97b22351ab22e9bd13fc6ad83b682b63b26d
vikas sharma <vikasjavadev311@gmail.com>
2025-10-02 19:35:19 +0530

    feat: integrate AWS Bedrock with Gemini (deepseek.v3-v1:0) in Spring Boot and configure chat API

---
+++ bedrock/bedrock/src/main/java/com/lcwv/bedrock/BedrockApplication.java
@@ -0,0 +1,13 @@
+package com.lcwv.bedrock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedrockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BedrockApplication.class, args);
+	}
+
+}
