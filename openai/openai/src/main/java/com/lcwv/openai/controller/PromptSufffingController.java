39cd478253c60cc7b9bf61bb233fdcc11f0363b2
vikas sharma <vikasjavadev311@gmail.com>
2025-10-05 17:34:06 +0530

    feat(ai): experiment with prompt stuffing and template limits
    - Added PromptSufffingController to test system prompt templates with user messages.
    - Using systemPromptTemplate.st to inject structured content for HR assistant.
    - Exploring limitations with prompt stuffing in ChatClient.

---
+++ openai/openai/src/main/java/com/lcwv/openai/controller/PromptSufffingController.java
@@ -0,0 +1,33 @@
+package com.lcwv.openai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PromptSufffingController {
+    private final ChatClient chatClient;
+
+    public PromptSufffingController(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @Value("classpath:/promptTemplates/systemPromptTemplate.st")
+    Resource systemPromptTemplate;
+
+    @GetMapping("/prompt-stuffing")
+    public String promptStuffing(@RequestParam("message") String message
+
+    ) {
+        return chatClient
+                .prompt()
+                .system(systemPromptTemplate)
+                .user(message)
+                .call().content();
+    }
+}
