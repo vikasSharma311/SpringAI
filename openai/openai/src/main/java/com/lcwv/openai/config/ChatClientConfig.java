967cb43c8f711403bc9227ef8d117114c2f84ea8
vikas sharma <vikasjavadev311@gmail.com>
2025-10-05 14:45:35 +0530

    feat(ai): explore ChatClient system and user role messages
    - Added ChatClient configuration with default HR assistant prompts.
    - Experimenting with system and user roles to set contextual defaults across the app.
    - Learning how to design consistent AI behavior through centralized prompt configuration.

---
+++ openai/openai/src/main/java/com/lcwv/openai/config/ChatClientConfig.java
@@ -0,0 +1,23 @@
+package com.lcwv.openai.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClientConfig {
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder){
+        return chatClientBuilder
+                .defaultSystem("""
+                        You are an HR Policy Assistant.
+                                    You must ONLY answer questions related to HR, leave policies, or company policy.
+                                    If the question is unrelated, respond strictly with:
+                                    "I'm sorry, I can only assist with HR and company leave policy related questions.
+                        """)
+                .defaultUser("""
+                        How can you help me ???
+                        """)
+                .build();
+    }
+}
--- openai/openai/src/main/java/com/lcwv/openai/controller/ChatController.java
+++ openai/openai/src/main/java/com/lcwv/openai/controller/ChatController.java
@@ -11,20 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
     private final ChatClient chatClient;
 
-    public ChatController(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
+    public ChatController(ChatClient chatClient) {
+        this.chatClient = chatClient;
     }
 
     @GetMapping("/chat")
     public String chat(@RequestParam("message") String message) {
-
         return chatClient
                 .prompt()
                 .system("""
-                            You are an HR Policy Assistant.
-                            You must ONLY answer questions related to HR, leave policies, or company policy.
-                            If the question is unrelated, respond strictly with:
-                            "I'm sorry, I can only assist with HR and company leave policy related questions."
+                        You are an internal IT helpdesk assistant. Your role is to assist\s
+                                                employees with IT-related issues such as resetting passwords,\s
+                                                unlocking accounts, and answering questions related to IT policies.
+                                                If a user requests help with anything outside of these\s
+                                                responsibilities, respond politely and inform them that you are\s
+                                                only able to assist with IT support tasks within your defined scope.
                         """)
                 .user(message)
                 .call().content();
