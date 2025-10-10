5170fb859b75cdcd353dae3affbaf5d4995de56e
vikas sharma <vikasjavadev311@gmail.com>
2025-10-10 21:37:31 +0530

    feat(ai): explore structured output converters for ChatClient responses
    - Added endpoints to handle AI responses as List, Map, and custom Bean types.
    - Used ListOutputConverter, MapOutputConverter, and BeanOutputConverter for structured parsing.
    - Learning how to flexibly convert ChatClient outputs into strongly typed Java objects.

--- openai/openai/src/main/java/com/lcwv/openai/controller/StructuredOutputController.java
+++ openai/openai/src/main/java/com/lcwv/openai/controller/StructuredOutputController.java
@@ -3,11 +3,17 @@ package com.lcwv.openai.controller;
 import com.lcwv.openai.model.CountryCities;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.converter.BeanOutputConverter;
+import org.springframework.ai.converter.ListOutputConverter;
+import org.springframework.ai.converter.MapOutputConverter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -26,6 +32,33 @@ public class StructuredOutputController {
                 .prompt()
                 .user(message)
                 .call().entity(CountryCities.class);
+        return ResponseEntity.ok(countryCities);
+    }
+    @GetMapping("/chat-list")
+    public ResponseEntity<List<String>> chatList(@RequestParam("message") String message) {
+        List<String> strings = chatClient
+                .prompt()
+                .user(message)
+                .call().entity(new ListOutputConverter());
+        return ResponseEntity.ok(strings);
+    }
+
+    @GetMapping("/chat-map")
+    public ResponseEntity<Map<String,Object>> chatMap(@RequestParam("message") String message) {
+        Map<String, Object> entity = chatClient
+                .prompt()
+                .user(message)
+                .call().entity(new MapOutputConverter());
+        return ResponseEntity.ok(entity);
+    }
+
+    //similar to this entity(CountryCities.class);
+    @GetMapping("/chat-bean-converter")
+    public ResponseEntity<CountryCities> chatBeanUsingConverter(@RequestParam("message") String message) {
+        CountryCities countryCities = chatClient
+                .prompt()
+                .user(message)
+                .call().entity(new BeanOutputConverter<>(CountryCities.class));
         return ResponseEntity.ok(countryCities);
     }
 }
