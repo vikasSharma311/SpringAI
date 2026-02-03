d6f4d171a1fb71137deec8368dc33002491f1a7e
vikas sharma <vikasjavadev311@gmail.com>
2026-02-03 23:20:57 +0530

    feat: add RAG controller with vector search and bootstrap sample data

---
+++ openai/openai/src/main/java/com/lcwv/openai/controller/RAGController.java
@@ -0,0 +1,46 @@
+package com.lcwv.openai.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
+
+@RestController
+@RequestMapping("/api/rag")
+public class RAGController {
+    private final ChatClient chatClient;
+    private final VectorStore vectorStore;
+
+    public RAGController(@Qualifier("chatMemoryChatClient") ChatClient chatClient, VectorStore vectorStore) {
+        this.chatClient = chatClient;
+        this.vectorStore = vectorStore;
+    }
+    @Value("classpath:/promptTemplates/systemPromptRandomDataTemplate.st")
+    Resource promptTemplate;
+
+    @GetMapping("/random/chat")
+    public ResponseEntity<String> randomChat(@RequestHeader("username") String username,
+                                             @RequestParam("message") String message) {
+        SearchRequest searchRequest = SearchRequest.builder().query(message).topK(3).similarityThreshold(0.5).build();
+        List<Document> documents = vectorStore.similaritySearch(searchRequest);
+        String similarContext = documents.stream().map(Document::getText).collect(Collectors.joining(System.lineSeparator()));
+        String answer = chatClient.prompt().system(promptSystemSpec -> promptSystemSpec.text(promptTemplate)
+                        .param("documents", similarContext))
+                .advisors(advisorSpec -> advisorSpec.param(CONVERSATION_ID, username))
+                .user(message)
+                .call().content();
+        return ResponseEntity.ok(answer);
+
+    }
+
+    }
