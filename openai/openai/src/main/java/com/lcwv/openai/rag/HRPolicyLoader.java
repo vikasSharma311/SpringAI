e6e7367f7e9a226633bbdde4c0c72c6e56734989
vikas sharma <vikasjavadev311@gmail.com>
2026-02-07 17:44:54 +0530

    feat: load HR policy PDF and store chunks in vector store for RAG

--- openai/openai/src/main/java/com/lcwv/openai/rag/HRPolicyLoader.java
+++ openai/openai/src/main/java/com/lcwv/openai/rag/HRPolicyLoader.java
@@ -3,6 +3,8 @@ package com.lcwv.openai.rag;
 import jakarta.annotation.PostConstruct;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -17,13 +19,15 @@ public class HRPolicyLoader {
     public HRPolicyLoader(VectorStore vectorStore) {
         this.vectorStore = vectorStore;
     }
+
     @Value("classpath:CODEWITHVIKAS_HR_POLICY_MANUAL.pdf")
     Resource policyFile;
+
     @PostConstruct
-    public void loadPDF(){
+    public void loadPDF() {
         TikaDocumentReader tikaDocumentReader = new TikaDocumentReader(policyFile);
         List<Document> documents = tikaDocumentReader.get();
-        vectorStore.add(documents);//witgh this there is a problem asd ve are not splitting the document in small chunks
-
+        TextSplitter textSplitter = TokenTextSplitter.builder().withChunkSize(100).withMaxNumChunks(400).build();
+        vectorStore.add(textSplitter.split(documents));
     }
 }
